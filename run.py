5674cff94ab447e3de0c09b9b2cc107f22c16dc7
Jayden Ontalan Capistrano <jaycapistrano01@gmail.com>
2025-10-05 23:17:51 -1000

    Changing run.py

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 import os
+import base64
 import uvicorn
+from main import create_app
+
+def write_rclone_config():
+    config_base64 = os.getenv("RCLONE_CONFIG_BASE64")
+    if config_base64:
+        os.makedirs("/root/.config/rclone", exist_ok=True)
+        with open("/root/.config/rclone/rclone.conf", "wb") as f:
+            f.write(base64.b64decode(config_base64))
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    write_rclone_config()
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(create_app, host="0.0.0.0", port=port)
