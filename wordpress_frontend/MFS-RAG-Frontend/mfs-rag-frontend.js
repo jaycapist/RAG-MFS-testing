df4771d5f6b6d47569b0d503e4d2f910ccdf93c1
Kyle Bueche <kyle.bueche@gmail.com>
2025-11-18 18:49:09 -1000

    Added initial barebones frontend

---
+++ wordpress_frontend/MFS-RAG-Frontend/mfs-rag-frontend.js
@@ -0,0 +1,66 @@
+/*
+Plugin Name: Minimal API
+Description: Minimal plugin with persistent input via REST API.
+Version: 1.0
+Author: Kyle Bueche
+
+Shortcode: [mfs_rag_frontend]
+*/
+
+async function queryragapi() {
+    let ragOutputText = document.getElementById("rag-output-generative-text");
+    ragOutputText.innerHTML = "Finding Documents...";
+    const input = document.getElementById("rag-input").value;
+
+    console.log("starting api fetch...");
+    const response = await fetch("https://rag-mfs-testing.onrender.com/query", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ query: input })
+    });
+    console.log("getting json...");
+    const data = await response.json();
+    console.log(data);
+    ragOutputText.innerHTML = data.answer;
+}
+function outputDocuments() {
+    ragOutputDocuments = document.getElementById("rag-output-documents");
+    documents = [];
+    documents.push({
+        docUrl: "https://drive.google.com/file/d/1outes8jvthnMdauDKI8c7f2zejPqkHwh/preview",
+        docTitle: "Senate_Aloha_Resolution"
+    });
+    documents.push({
+        docUrl: "https://drive.google.com/file/d/1outes8jvthnMdauDKI8c7f2zejPqkHwh/preview",
+        docTitle: "Another_Resolution"
+    });
+    documents.push({
+        docUrl: "https://drive.google.com/file/d/1outes8jvthnMdauDKI8c7f2zejPqkHwh/preview",
+        docTitle: "Senate_Document"
+    });
+    documents.push({
+        docUrl: "https://drive.google.com/file/d/1outes8jvthnMdauDKI8c7f2zejPqkHwh/preview",
+        docTitle: "Senate_Example_Resolution"
+    });
+    documents.push({
+        docUrl: "https://drive.google.com/file/d/1outes8jvthnMdauDKI8c7f2zejPqkHwh/preview",
+        docTitle: "Senate_Example_Resolution_2"
+    });
+
+    for (let i = 0; i < documents.length; i++) {
+        const documentEntry = `
+        <a target="_blank" href="${documents[i].docUrl}" class="document-entry">${documents[i].docTitle}</a>
+        `
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = documentEntry;
+        ragOutputDocuments.appendChild(wrapper);
+    }
+};
+
+window.onload = function() {
+    document.getElementById("myButton").onclick = function () {
+        outputDocuments();
+    };
+}
---
+++ wordpress_frontend/MFS-RAG-Frontend/mfs-rag-frontend.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: Minimal API
+Description: Minimal plugin with persistent input via REST API.
+Version: 1.0
+Author: Kyle Bueche
+
+Shortcode: [mfs_rag_frontend]
+*/
+
+if (!defined('ABSPATH')) exit;
+
+/*
+Injects HTML & CSS into the shortcode: [mfs_rag_frontend].
+Runs JavaScript code once all the HTML has been loaded.
+*/
+function rag_frontend( $atts ) {
+    load_js_from_file();
+    load_css_from_file();
+    return load_html_from_file();
+}
+
+/*
+Load the JavaScript.
+Function form:
+wp_enqueue_script(handle, src, deps, ver, args[strategy, in_footer])
+*/
+function load_js_from_file() {
+    wp_enqueue_script(
+        'mfs-rag-frontend-script',
+        get_template_directory_uri() . 'mfs-rag-frontend.js',
+        array(),
+        '1.0.0',
+        array(
+            'strategy' => defer, // Only run once DOM tree has fully loaded
+            'in_footer' => false
+        )
+    );
+}
+
+/*
+Load the CSS stylesheet.
+Function form:
+wp_enqueue_script(handle, src, deps, ver, media)
+*/
+function load_css_from_file() {
+    wp_enqueue_style(
+        'mfs-rag-frontend-style',
+        get_template_directory_uri() . 'mfs-rag-frontend.css',
+        array(),
+        '1.0.0',
+        'all' // Defined for all media types
+    );
+}
+
+/*
+Parses HTML from file, and returns it as what the shortcode expects.
+*/
+function load_html_from_file() {
+    ob_start();
+    include( get_template_directory() . 'mfs-rag-frontend.html' );
+    $html_content = ob_get_clean();
+    return $html_content;
+}
+
+// Register the shortcode
+add_shortcode( 'mfs_rag_frontend', 'rag_frontend' );
